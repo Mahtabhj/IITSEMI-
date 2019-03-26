1368af49d489ace490a1b5a1bdf0e92b989be6fb
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2019-03-26 18:03:02 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
-
 #include"header.h"
 
 
 int main()
 {
-    printf("Welcome to the IIT Student Information System \n");
-    printf("Input the information");
+    printf("----------Welcome to the IIT Student Information System--------- \n");
     f1();
     menu();
     return 0;
