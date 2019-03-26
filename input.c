6c1e34b0d1f8e010da0c1ea501d765b1761316ec
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2019-03-26 17:58:50 +0600

    Update input.c

--- input.c
+++ input.c
@@ -1,7 +1,7 @@
 #include"header.h"
 void f1()
 {
-    int i;FILE *f;
+    int i,j;FILE *f;
     f=fopen("info.txt","r");
     if(f==NULL)printf("no file");
     else {printf("\nfile open");}
@@ -9,5 +9,9 @@ void f1()
     {
         printf("\n%d student name = ",i+1);
         fscanf(f,"%s %d",&st[i].name,&st[i].roll);
+        for(j=0;j<6;j++)
+        {
+            fscanf(f,"%d",&st[i].marks[j]);
+        }
     }
 }
