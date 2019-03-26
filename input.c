764b29bff24a3da20c69c60ffa81dab901d3b7a7
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2019-03-26 18:04:25 +0600

    Update input.c

--- input.c
+++ input.c
@@ -3,11 +3,8 @@ void f1()
 {
     int i,j;FILE *f;
     f=fopen("info.txt","r");
-    if(f==NULL)printf("no file");
-    else {printf("\nfile open");}
     for(i=0;i<4;i++)
     {
-        printf("\n%d student name = ",i+1);
         fscanf(f,"%s %d",&st[i].name,&st[i].roll);
         for(j=0;j<6;j++)
         {
