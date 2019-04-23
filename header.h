55797319652ac59a4641e905a88bbdb29689d1e4
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2019-04-23 20:42:52 +0600

    Update header.h

--- header.h
+++ header.h
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 struct st {
-       char name[20];
-       int roll;
-       int marks[6];
-}st[4];
+       char name[20],address[20],roll[10];
+       int reg,serial;
+       int marks[8][6];
+}st[128];
 void f1();
 void menu();
+void cg();
+void infout();
+void cg();
+void infoutR();
+void infoutR();
+void infoutR();
+void cgoutR();
+void cgoutn();
+void cgoutR();
