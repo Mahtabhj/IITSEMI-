e397de52a7a77902e8030a77dbef5326cd142642
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2019-04-24 23:02:10 +0600

    Update pout.c

--- pout.c
+++ pout.c
@@ -2,17 +2,13 @@
 void infout()
 {
     int c;
-    printf("If you have the ROLL press    = 1\nIf you have the NAME press = 2\nIf you have the REGISTRATION NUMBER press = 3\n");
+    printf("If you have the ROLL press    = 1\nIf you have the REGISTRATION NUMBER press = 3\n");
     scanf("%d",&c);
     if(c==1)
     {
         infoutR();
     }
     else if(c==2)
-    {
-        infoutn();
-    }
-    else if(c==3)
     {
         infoutr();
     }
@@ -21,17 +17,13 @@ void infout()
 void cg()
 {
     int c;
-    printf("If you have the ROLL press    = 1\nIf you have the NAME press = 2\nIf you have the REGISTRATION NUMBER press = 3\n");
+    printf("If you have the ROLL press    = 1\nIf you have the REGISTRATION NUMBER press = 2\n");
     scanf("%d",&c);
      if(c==1)
     {
         cgoutR();
     }
     else if(c==2)
-    {
-        cgoutn();
-    }
-    else if(c==3)
     {
         cgoutr();
     }
