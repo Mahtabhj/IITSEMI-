7af338d949b7005351ef76b515776b0dd2e74b8e
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2019-04-24 23:00:21 +0600

    Update io.c

--- io.c
+++ io.c
@@ -13,20 +13,6 @@ void infoutR()
             }
         }
 }
-void infoutn()
-{
-    int i;char n[20];
-    printf("Enter the name = ");
-    scanf("%s",n);
-     for(i=0;i<128;i++)
-        {
-            if(strcmp(st[i].name,n)==0)
-            {
-                printf("\n    INFORMATION \nName = %s\nAddress  = %s\nRoll  = %s\n",st[i].name,st[i].address,st[i].roll);
-                break ;
-            }
-        }
-}
 void infoutr()
 {
 
@@ -96,73 +82,14 @@ void cgoutR()
             menu();}
 
             }
-            printf("Cgpa of semister no %d   = %.2f\n",s+1,c/6);
+            printf("Name     = %s\nCgpa of semister no %d   = %.2f\n",st[i].name,s+1,c/6);
             break;
         }
         if(d==1){break;}
     }
     menu();
 }
-void cgoutn()
-{
-    int i,j,d=0,s;char n[10];
-    float c=0;
-    printf("Enter the name  = ");scanf("%s",&n);
-    printf("Enter the semister number = ");scanf("%d ",&s);s=s-1;
-         for(i=0;i<128;i++)
-        {
-            if(strcmp(st[i].name,n)==0)
-            {d=1;
-            for(j=0;j<6;j++)
-            {
 
-                if(st[i].marks[s][j]>=80)
-            {
-                c=c+4.00;
-            }
-            else if(st[i].marks[s][j]>=75&&st[i].marks[s][j]<80)
-            {
-                c=c+3.75;
-            }
-            else if(st[i].marks[s][j]>=70&&st[i].marks[s][j]<75)
-            {
-                c=c+3.50;
-            }
-            else if(st[i].marks[s][j]>=65&&st[i].marks[s][j]<70)
-            {
-               c=c+3.25;
-            }
-            else if(st[i].marks[s][j]>=60&&st[i].marks[s][j]<65)
-            {
-                c=c+3.00;
-            }
-            else if(st[i].marks[s][j]>=55&&st[i].marks[s][j]<60)
-            {
-                c=c+2.75;
-            }
-            else if(st[i].marks[s][j]>=50&&st[i].marks[s][j]<55)
-            {
-               c=c+2.50;
-            }
-            else if(st[i].marks[s][j]>=45&&st[i].marks[s][j]<50)
-            {
-               c=c+2.25;
-            }
-            else if(st[i].marks[s][j]>=40&&st[i].marks[s][j]<45)
-            {
-               c=c+2.00;
-            }
-            else {printf("Got %d \nFailed\n",st[i].marks[s][j]);
-            menu();}
-
-            }
-            printf("Cgpa of semister no %d   = %.2f\n",s+1,c/6);
-            break;
-        }
-        if(d==1){break;}
-    }
-    menu();
-}
 void cgoutr()
 {
     int i,j,d=0,s,r;
@@ -216,10 +143,89 @@ void cgoutr()
             menu();}
 
             }
-            printf("Cgpa of semister no %d   = %.2f\n",s+1,c/6);
+            printf("Name     = %s\nCgpa of semister no %d   = %.2f\n",st[i].name,s+1,c/6);
             break;
         }
         if(d==1){break;}
     }
     menu();
+}
+void cgb()
+{
+    int b,i,s,j,k;float c=0;
+    printf("Enter batch no = ");
+    scanf("%d",&b);
+    printf("Enter semister no = ");
+    scanf("%d",&s);s=s-1;
+    if(b==11){i=0;k=32;}
+    else if (b==10){i=32;k=64;}
+    else if (b==9){i=64;k=96;}
+    else if (b==10){i=96;k=128;}
+       for(;i<k;i++)
+        { c=0;
+            printf("Name = %s\t\tRoll = %s\t\t\t",st[i].name,st[i].roll);
+            for(j=0;j<6;j++)
+            {
+
+                if(st[i].marks[s][j]>=80)
+            {
+                c=c+4.00;
+            }
+            else if(st[i].marks[s][j]>=75&&st[i].marks[s][j]<80)
+            {
+                c=c+3.75;
+            }
+            else if(st[i].marks[s][j]>=70&&st[i].marks[s][j]<75)
+            {
+                c=c+3.50;
+            }
+            else if(st[i].marks[s][j]>=65&&st[i].marks[s][j]<70)
+            {
+               c=c+3.25;
+            }
+            else if(st[i].marks[s][j]>=60&&st[i].marks[s][j]<65)
+            {
+                c=c+3.00;
+            }
+            else if(st[i].marks[s][j]>=55&&st[i].marks[s][j]<60)
+            {
+                c=c+2.75;
+            }
+            else if(st[i].marks[s][j]>=50&&st[i].marks[s][j]<55)
+            {
+               c=c+2.50;
+            }
+            else if(st[i].marks[s][j]>=45&&st[i].marks[s][j]<50)
+            {
+               c=c+2.25;
+            }
+            else if(st[i].marks[s][j]>=40&&st[i].marks[s][j]<45)
+            {
+               c=c+2.00;
+            }
+            else {printf("Got %d \nFailed\n",st[i].marks[s][j]);
+            break;;}
+
+            }
+            printf("Cgpa = %.2f\n",c/6);
+
+        }
+    }
+void cgc()
+{
+    int i,s,j,k,b;
+    printf("Enter the batch no = ");
+    scanf("%d",&b);
+    printf("Enter semister no = ");
+    scanf("%d",&s);s=s-1;
+    printf("For course 101 press = 1\ncourse 102 press = 2\ncourse 103 press = 3\ncourse 104 press = 4\ncourse 105 press = 5\ncourse 106 press = 6\n");
+    scanf("%d",&j);j=j-1;
+    if(b==11){i=0;k=32;}
+    else if (b==10){i=32;k=64;}
+    else if (b==9){i=64;k=96;}
+    else if (b==10){i=96;k=128;}
+    for(;i<k;i++)
+    {
+        printf("Name = %s\t\tMarks =%d\n",st[i].name,st[i].marks[s][j]);
+    }
 }
