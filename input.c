dbe8352f9f9a62b2c5e55bf521f51c182a073d99
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2019-05-01 23:39:08 +0600

    Update input.c

--- input.c
+++ input.c
@@ -3,11 +3,12 @@ void f1()
 {
     int i,j,k;FILE *f;
     f=fopen("info.txt","r");
-    for(i=0;i<32;i++)
+    for(i=0;i<128;i++)
     {
         fscanf(f,"%d ",&st[i].serial);
         fgets(st[i].name,20,f);
-        fscanf(f,"%s %s %d",&st[i].address,&st[i].roll,&st[i].reg);
+        fgets(st[i].address,20,f);
+        fscanf(f,"%s %d",&st[i].roll,&st[i].reg);
         for(j=0;j<8;j++)
         {
             for(k=0;k<6;k++)
@@ -16,4 +17,5 @@ void f1()
             }
         }
     }
+
 }
