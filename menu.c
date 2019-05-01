c80a5ec1554610744f58b3afccafe4b75bc8cf56
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2019-05-01 23:40:39 +0600

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,17 +2,17 @@
 #include"header.h"
 void menu()
 {
-    printf("\nIf you want to know the information press : Y\nIf you want to know cgpa press            : R\n");
+    printf("\n\t\tFor the information press : I\n\t\tFor the result            : R\n");
     char c;
     int r,i;
     scanf(" %c",&c);
-    if (c=='Y')
+    if (c=='I')
     {
        infout();
     }
     else if(c=='R')
     {
-        printf("For individual Batch result press = 1\n         Student result press = 2\nMarks of an individual course press = 3\n");
+        printf("\t\tFor individual Batch result    press   = 1\n\t\tFor individual student result          = 2\n\t\tMarks of an individual course          = 3\n");
         scanf("%d",&r);
         if(r==1)
         {
